b4ab88f69ca21e448400d2a702227c82ddf05e87
ahmed <154706300+AhmedNabil2o01@users.noreply.github.com>
2024-03-26 00:13:08 +0200

    Update Home.jsx

--- src/Component/Home/Home.jsx
+++ src/Component/Home/Home.jsx
@@ -4,99 +4,99 @@ export default function Home() {
   return <>
   <div className="row">
     <div className="col-md-3 text-center">
-    <i class="fa-solid fa-2x fa-city"></i>
+    <i class="fa-solid fa-3x fa-city"></i>
     <h5>title</h5>
     </div>
     <div className="col-md-3 text-center">
-    <i class="fa-solid fa-2x fa-city"></i>
+    <i class="fa-solid fa-3x fa-city"></i>
     <h5>title</h5>
     </div>
     <div className="col-md-3 text-center">
-    <i class="fa-solid fa-2x fa-city"></i>
+    <i class="fa-solid fa-3x fa-city"></i>
     <h5>title</h5>
     </div>
     <div className="col-md-3 text-center">
-    <i class="fa-solid fa-2x fa-city"></i>
+    <i class="fa-solid fa-3x fa-city"></i>
     <h5>title</h5>
     </div>
     <div className="col-md-3 text-center">
-    <i class="fa-solid fa-2x fa-city"></i>
+    <i class="fa-solid fa-3x fa-city"></i>
     <h5>title</h5>
     </div>
     <div className="col-md-3 text-center">
-    <i class="fa-solid fa-2x fa-city"></i>
+    <i class="fa-solid fa-3x fa-city"></i>
     <h5>title</h5>
     </div>
     <div className="col-md-3 text-center">
-    <i class="fa-solid fa-2x fa-city"></i>
+    <i class="fa-solid fa-3x fa-city"></i>
     <h5>title</h5>
     </div>
     <div className="col-md-3 text-center">
-    <i class="fa-solid fa-2x fa-city"></i>
+    <i class="fa-solid fa-3x fa-city"></i>
     <h5>title</h5>
     </div>
     <div className="col-md-3 text-center">
-    <i class="fa-solid fa-2x fa-city"></i>
+    <i class="fa-solid fa-3x fa-city"></i>
     <h5>title</h5>
     </div>
     <div className="col-md-3 text-center">
-    <i class="fa-solid fa-2x fa-city"></i>
+    <i class="fa-solid fa-3x fa-city"></i>
     <h5>title</h5>
     </div>
     <div className="col-md-3 text-center">
-    <i class="fa-solid fa-2x fa-city"></i>
+    <i class="fa-solid fa-3x fa-city"></i>
     <h5>title</h5>
     </div>
     <div className="col-md-3 text-center">
-    <i class="fa-solid fa-2x fa-city"></i>
+    <i class="fa-solid fa-3x fa-city"></i>
     <h5>title</h5>
     </div>
     <div className="col-md-3 text-center">
-    <i class="fa-solid fa-2x fa-city"></i>
+    <i class="fa-solid fa-3x fa-city"></i>
     <h5>title</h5>
     </div>
     <div className="col-md-3 text-center">
-    <i class="fa-solid fa-2x fa-city"></i>
+    <i class="fa-solid fa-3x fa-city"></i>
     <h5>title</h5>
     </div>
     <div className="col-md-3 text-center">
-    <i class="fa-solid fa-2x fa-city"></i>
+    <i class="fa-solid fa-3x fa-city"></i>
     <h5>title</h5>
     </div>
     <div className="col-md-3 text-center">
-    <i class="fa-solid fa-2x fa-city"></i>
+    <i class="fa-solid fa-3x fa-city"></i>
     <h5>title</h5>
     </div>
     <div className="col-md-3 text-center">
-    <i class="fa-solid fa-2x fa-city"></i>
+    <i class="fa-solid fa-3x fa-city"></i>
     <h5>title</h5>
     </div>
     <div className="col-md-3 text-center">
-    <i class="fa-solid fa-2x fa-city"></i>
+    <i class="fa-solid fa-3x fa-city"></i>
     <h5>title</h5>
     </div>
     <div className="col-md-3 text-center">
-    <i class="fa-solid fa-2x fa-city"></i>
+    <i class="fa-solid fa-3x fa-city"></i>
     <h5>title</h5>
     </div>
     <div className="col-md-3 text-center">
-    <i class="fa-solid fa-2x fa-city"></i>
+    <i class="fa-solid fa-3x fa-city"></i>
     <h5>title</h5>
     </div>
     <div className="col-md-3 text-center">
-    <i class="fa-solid fa-2x fa-city"></i>
+    <i class="fa-solid fa-3x fa-city"></i>
     <h5>title</h5>
     </div>
     <div className="col-md-3 text-center">
-    <i class="fa-solid fa-2x fa-city"></i>
+    <i class="fa-solid fa-3x fa-city"></i>
     <h5>title</h5>
     </div>
     <div className="col-md-3 text-center">
-    <i class="fa-solid fa-2x fa-city"></i>
+    <i class="fa-solid fa-3x fa-city"></i>
     <h5>title</h5>
     </div>
     <div className="col-md-3 text-center">
-    <i class="fa-solid fa-2x fa-city"></i>
+    <i class="fa-solid fa-3x fa-city"></i>
     <h5>title</h5>
     </div>
 
