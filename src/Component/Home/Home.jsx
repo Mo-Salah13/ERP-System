45b41de1456d7d610b8181d6f031527c423751e6
ahmed <154706300+AhmedNabil2o01@users.noreply.github.com>
2024-03-26 00:21:05 +0200

    Update Home.jsx

--- src/Component/Home/Home.jsx
+++ src/Component/Home/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Home() {
   return <>
-  <div className="row">
+  <div className="row g-5">
     <div className="col-md-3 text-center">
     <i class="fa-solid fa-3x fa-city"></i>
     <h5>title</h5>
