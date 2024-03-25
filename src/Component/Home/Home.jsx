0ea95f241a0a5d9064eff3778c4b9f9c08e5de5a
ahmed <154706300+AhmedNabil2o01@users.noreply.github.com>
2024-03-26 00:24:08 +0200

    Update Home.jsx

--- src/Component/Home/Home.jsx
+++ src/Component/Home/Home.jsx
@@ -67,39 +67,6 @@ export default function Home() {
     <i class="fa-solid fa-3x fa-city"></i>
     <h5>title</h5>
     </div>
-    <div className="col-md-3 text-center">
-    <i class="fa-solid fa-3x fa-city"></i>
-    <h5>title</h5>
-    </div>
-    <div className="col-md-3 text-center">
-    <i class="fa-solid fa-3x fa-city"></i>
-    <h5>title</h5>
-    </div>
-    <div className="col-md-3 text-center">
-    <i class="fa-solid fa-3x fa-city"></i>
-    <h5>title</h5>
-    </div>
-    <div className="col-md-3 text-center">
-    <i class="fa-solid fa-3x fa-city"></i>
-    <h5>title</h5>
-    </div>
-    <div className="col-md-3 text-center">
-    <i class="fa-solid fa-3x fa-city"></i>
-    <h5>title</h5>
-    </div>
-    <div className="col-md-3 text-center">
-    <i class="fa-solid fa-3x fa-city"></i>
-    <h5>title</h5>
-    </div>
-    <div className="col-md-3 text-center">
-    <i class="fa-solid fa-3x fa-city"></i>
-    <h5>title</h5>
-    </div>
-    <div className="col-md-3 text-center">
-    <i class="fa-solid fa-3x fa-city"></i>
-    <h5>title</h5>
-    </div>
-
   </div>
   
   </>
